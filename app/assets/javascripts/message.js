2db7366d99f5208282f6c43da99e9870d6b664b4
kirio4o7 <hkirikihira@gmail.com>
2020-04-30 17:05:41 +0900

    Chatspace自動更新機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
 
   function buildHTML(message){
     if ( message.image ) {
-      var html = `<div class="message">
+      var html = `<div class="message" data-message-id=${message.id}>
                     <div class="main-message__group">
                       <div class="main-message__name">
                         ${message.user_name}
@@ -18,7 +18,7 @@ $(function(){
                   </div>`
       return html;
     } else {
-      var html = `<div class="message">
+      var html = `<div class="message" data-message-id=${message.id}>
                     <div class="main-message__group">
                       <div class="main-message__name">
                         ${message.user_name}
@@ -60,4 +60,29 @@ $(function(){
     });
     return false;
   })
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main-messages').append(insertHTML);
+        $('.main-messages').animate({ scrollTop: $('.main-messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error')
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
