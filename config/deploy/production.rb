e6413922a10b21a82db2b4b3c35be45597b3c2ed
kirio4o7 <hkirikihira@gmail.com>
2020-05-07 17:12:17 +0900

    AWS#6

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '18.177.6.39', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -59,4 +61,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '18.177.6.39', user: 'ec2-user', roles: %w{app db web}
+
